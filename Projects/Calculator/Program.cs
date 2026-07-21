bb2a6235883f69a582d625201556627d1a651be7
LeS-csharp <savinlev2011@gmail.com>
2026-07-21 16:19:25 +0700

    Update Program.cs
    2nd version, I added new class  named "Calculator" to organize code and use more functios later

--- Projects/Calculator/Program.cs
+++ Projects/Calculator/Program.cs
@@ -1,5 +1,4 @@
-// НАПИСАЛ 1 ВЕРСИЮ С ПОДСЧЕТОМ(БЕЗ СОХРАНЕНИЯ И ИСТОРИИ)
-
+// НАПИСАЛ 2 ВЕРСИЮ С ПОДСЧЕТОМ И РАЗДЕЛИЛ ЕГО НА ДВА КЛАССА(БЕЗ СОХРАНЕНИЯ И ИСТОРИИ)
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,15 +13,34 @@ namespace GitWork
         static void Main(string[] args)
         {
             Console.WriteLine("Введите операцию типа [x - y]");
-            Operations solution = new Operations();
+            Calculator solution = new Calculator();
             char oper = solution.StrToNums();
-            if (oper == '+') { solution.Plus(); }
-            if (oper == '-') { solution.Minus(); }
-            if (oper == '*') { solution.Multiply(); }
-            if (oper == '/') { solution.Divide(); }
         }
     }
     class Operations
+    {
+        public void Plus(double Fn, double Sn)
+        {
+            Console.WriteLine($"{Fn} + {Sn} = {Fn+Sn}");
+        }
+
+        public void Minus(double Fn, double Sn)
+        {
+            Console.WriteLine($"{Fn} - {Sn} = {Fn - Sn}");
+        }
+
+        public void Multiply(double Fn, double Sn)
+        {
+            Console.WriteLine($"{Fn} * {Sn} = {Fn * Sn}");
+        }
+
+        public void Divide(double Fn, double Sn)
+        {
+            Console.WriteLine(Sn == 0 ? "Не получится)" : $"{ Fn} / {Sn} = {Fn / Sn}");
+        }
+    }
+
+    class Calculator
     {
         double Fn;
         double Sn;
@@ -32,60 +50,42 @@ namespace GitWork
         public char StrToNums()
         {
             allPath = Console.ReadLine();
+            Operations oper = new Operations();
 
             if (allPath.Contains("+"))
             {
-                operate = '+';
                 paths = allPath.Split('+');
                 Fn = Convert.ToDouble(paths[0]);
                 Sn = Convert.ToDouble(paths[1]);
+                oper.Plus(Fn, Sn);
             }
 
             else if (allPath.Contains("-"))
             {
-                operate = '-';
                 paths = allPath.Split('-');
                 Fn = Convert.ToDouble(paths[0]);
                 Sn = Convert.ToDouble(paths[1]);
+                oper.Minus(Fn, Sn);
             }
 
             else if (allPath.Contains("*"))
             {
-                operate = '*';
                 paths = allPath.Split('*');
                 Fn = Convert.ToDouble(paths[0]);
                 Sn = Convert.ToDouble(paths[1]);
+                oper.Multiply(Fn, Sn);
             }
 
             else if (allPath.Contains("/"))
             {
-                operate = '/';
                 paths = allPath.Split('/');
                 Fn = Convert.ToDouble(paths[0]);
                 Sn = Convert.ToDouble(paths[1]);
+                oper.Divide(Fn, Sn);
             }
             else { Console.WriteLine("Операция не найдена"); }
             return operate;
         }
-
-        public void Plus()
-        {
-            Console.WriteLine($"{Fn} + {Sn} = {Fn+Sn}");
-        }
-
-        public void Minus()
-        {
-            Console.WriteLine($"{Fn} - {Sn} = {Fn - Sn}");
-        }
-
-        public void Multiply()
-        {
-            Console.WriteLine($"{Fn} * {Sn} = {Fn * Sn}");
-        }
-
-        public void Divide()
-        {
-            Console.WriteLine(Sn == 0 ? "Не получится)" : $"{ Fn} / {Sn} = {Fn / Sn}");
-        }
     }
 }
+
