f3aa3a65f8ee1f929ec17de58638c7366cf1669f
LeS-csharp <savinlev2011@gmail.com>
2026-07-26 15:19:46 +0700

    Update Program.cs
    feat: add menu and history foundation

--- Projects/Calculator/Program.cs
+++ Projects/Calculator/Program.cs
@@ -1,7 +1,10 @@
-// НАПИСАЛ 2 ВЕРСИЮ С ПОДСЧЕТОМ И РАЗДЕЛИЛ ЕГО НА ДВА КЛАССА(БЕЗ СОХРАНЕНИЯ И ИСТОРИИ)
+// НАПИСАЛ 3 ВЕРСИЮ, ДОБАВИЛ МЕНЮ И УЛУЧШИЛ ИНТЕРФЕЙС
 using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Services;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,9 +15,34 @@ namespace GitWork
         // изучил классы и решил добавить в проект
         static void Main(string[] args)
         {
+            Enter enter = new Enter();
+            enter.Start();
+            enter.CAlculator();
+        }
+    }
+
+    class Enter
+    {
+        public void Start()
+        {
+            Console.WriteLine("Выберите:");
+            Console.WriteLine("1. Калькулятор");
+            Console.WriteLine("2. История вычислений");
+            Console.WriteLine();
+            if (Console.ReadLine() == "1") { CAlculator(); }
+            else if (Console.ReadLine() == "2") { History(); }
+        }
+
+        public void CAlculator()
+        {
+            Console.Clear();
             Console.WriteLine("Введите операцию типа [x - y]");
             Calculator solution = new Calculator();
-            char oper = solution.StrToNums();
+            solution.StrToNums();
+        }
+
+        public void History()
+        {
         }
     }
     class Operations
@@ -44,13 +72,13 @@ namespace GitWork
     {
         double Fn;
         double Sn;
-        char operate;
         string allPath;
         string[] paths;
-        public char StrToNums()
+        public void StrToNums()
         {
             allPath = Console.ReadLine();
             Operations oper = new Operations();
+            Enter enter = new Enter();
 
             if (allPath.Contains("+"))
             {
@@ -83,9 +111,14 @@ namespace GitWork
                 Sn = Convert.ToDouble(paths[1]);
                 oper.Divide(Fn, Sn);
             }
-            else { Console.WriteLine("Операция не найдена"); }
-            return operate;
+            else 
+            { 
+                Console.WriteLine("Операция не найдена");
+            }
+            Console.WriteLine("Нажмите любую клавишу чтобы перейти в меню...");
+            Console.ReadKey();
+            Console.Clear();
+            enter.Start();
         }
     }
 }
-
