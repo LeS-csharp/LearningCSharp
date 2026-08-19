dc3e84383574fe28880ca649dd4efa17ef6442c0
LeS-csharp <savinlev2011@gmail.com>
2026-08-19 16:38:41 +0700

    VERS-1
    Added some text to the start

--- Projects/PhoneBook/Program.cs
+++ Projects/PhoneBook/Program.cs
@@ -1,1 +1,26 @@
-..НЕТ ТЕКСТА..
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitWork
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**ТЕЛЕФОННАЯ КНИГА**");
+            Console.WriteLine("--------------------");
+            Console.WriteLine("1-добавить контакт");
+            Console.WriteLine("2-удалить контакт");
+            Console.WriteLine("3-найти по названию");
+            Console.WriteLine("--------------------");
+            string input = Console.ReadLine();
+        }
+    }
+}
